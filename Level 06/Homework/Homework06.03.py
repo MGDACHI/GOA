09f89861722b2da0ac5ca811ad081fd4baca0284
MGDACHI <dachidzagnidze2009@gmail.com>
2024-05-28 21:23:04 +0400

    second commit

---
+++ Level 06/Homework/Homework06.03.py
@@ -0,0 +1,9 @@
+ # შევქმნათ მარტივი პროგრამა სადაც მომხმარებელს ვკითხავთ მონაცემებს
+
+name = input("miutitet tqveni saxeli")
+
+congratulatory_letter = input("daweret milocvis werili")
+
+
+# ბოლოს როდესაც პროგრამა დასრულდება გამოვიყვანოთ ტერმინალში რომ პროგრამა დასრულდა
+print("tqven warmatebit daweretmisaloci barati")
