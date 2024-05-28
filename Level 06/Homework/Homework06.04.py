6bb17839cbd90b2fead59329b5dce8a31cfa62a8
MGDACHI <dachidzagnidze2009@gmail.com>
2024-05-28 21:55:27 +0400

    second commit

--- Level 06/Homework/Homework06.04.py
+++ Level 06/Homework/Homework06.04.py
@@ -4,3 +4,4 @@ name = input("miutitet tqveni saxeli")
 surname = input("miutitet tqveni gvari")
 age = input("miutitet tqveni asaki")
 
+print("tqven sheikvaneT tqveni monacemebi" + name + surname + age)
